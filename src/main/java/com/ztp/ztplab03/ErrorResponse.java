5f8ea1f063ea34469f2cec8e3936fef2d115497e
mateusz.pluzek <mateusz.pluz@gmail.com>
2025-03-10 00:32:15 +0100

    Added Put and delete

---
+++ src/main/java/com/ztp/ztplab03/ErrorResponse.java
@@ -0,0 +1,27 @@
+package com.ztp.ztplab03;
+
+public class ErrorResponse {
+    private String error;
+    private int status;
+
+    public ErrorResponse(String error, int status) {
+        this.error = error;
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
--- src/main/java/com/ztp/ztplab03/UserController.java
+++ src/main/java/com/ztp/ztplab03/UserController.java
@@ -1,6 +1,7 @@
 package com.ztp.ztplab03;
 
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,7 +25,34 @@ public class UserController {
         return repository.save(newUser);
     }
 
-
+    //GET
+    @GetMapping("/users/{id}")
+    User getStation(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(()->new UserNotFoundException());
+    }
+    //UPDATE
+    //update
+    @PutMapping("/users/{id}")
+    User updateStation(@Valid @RequestBody User newUser, @PathVariable Integer id) {
+        return repository.findById(id).map(
+                user -> {
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return repository.save(user);
+                }
+        ).orElseThrow(() -> new UserNotFoundException());
+    }
+    //DELETE
+    @DeleteMapping("/users/{id}")
+    ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+       if(repository.existsById(id)) {
+           repository.deleteById(id);
+           return ResponseEntity.noContent().build();
+       }
+       else {
+           return ResponseEntity.notFound().build();
+       }
+    }
 
 
 }
